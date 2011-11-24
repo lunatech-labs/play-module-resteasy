45bd3e2c0dcc3276ab117de750a76cf5fffe0da7
Stéphane Thomas <sthomas@lunatech.com>
2011-11-24 13:55:21 +0100

    Fixed a NPE raised when a wrong XML parser is used (which is the case when another module defining another parser is loaded before this module in the classpath). Also formatted code.

--- app/controllers/RESTEasyController.java
+++ app/controllers/RESTEasyController.java
@@ -1,10 +1,8 @@
 package controllers;
-
 import org.jboss.resteasy.core.Dispatcher;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.HttpResponse;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
-
 import play.Logger;
 import play.Play;
 import play.modules.resteasy.RESTEasyPlugin;
@@ -12,14 +10,23 @@ import play.modules.resteasy.RESTEasyRequestWrapper;
 import play.modules.resteasy.RESTEasyResponseWrapper;
 import play.mvc.Controller;
 
+
+
 public class RESTEasyController extends Controller {
-	public static void serve(){
-		Logger.info("RESTEasy controller invoked: %s", request.url);
-		RESTEasyPlugin plugin = Play.plugin(RESTEasyPlugin.class);
-		Dispatcher dispatcher = plugin.deployment.getDispatcher();
-		ResteasyProviderFactory factory = plugin.deployment.getProviderFactory();
-		HttpRequest restReq = new RESTEasyRequestWrapper(request, plugin.path);
-		HttpResponse restRep = new RESTEasyResponseWrapper(request, response, factory);
-		dispatcher.invoke(restReq, restRep);
-	}
+  public static void serve() {
+    Logger.info("RESTEasy controller invoked: %s", request.url);
+    
+    RESTEasyPlugin plugin = Play.plugin(RESTEasyPlugin.class);
+    Dispatcher dispatcher = plugin.deployment.getDispatcher();
+    ResteasyProviderFactory factory = plugin.deployment.getProviderFactory();
+    
+    HttpRequest restReq = new RESTEasyRequestWrapper(request, plugin.path);
+    HttpResponse restRep = new RESTEasyResponseWrapper(request, response, factory);
+    
+    // Makes sure the right JAXB parser is loaded, since modules which might reference different parsers (in their 
+    // META-INF section) are loaded in a random order by Play! (i.e. Play! classpath order is always undefined) 
+    System.setProperty("javax.xml.parsers.SAXParserFactory", "org.apache.xerces.jaxp.SAXParserFactoryImpl");
+    
+    dispatcher.invoke(restReq, restRep);
+  }
 }
